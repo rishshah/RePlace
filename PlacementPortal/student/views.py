1203bcdde3acb15a9e2cf660851485199e3abf22
rrriiissshhh <rishrocks17@gmail.com>
2017-11-04 16:55:37 +0530

    spell bug fixed

--- PlacementPortal/student/views.py
+++ PlacementPortal/student/views.py
@@ -12,9 +12,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def login(request):
-	print("XXX1")
 	if request.POST :
-		print("XXX2")
 		user = authenticate(username=request.POST['username'], password=request.POST['password'])
 		if user is not None:  # A backend authenticated the credentials
 			if user.is_active:
