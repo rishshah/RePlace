1d943321189366722461211a759e8ae1197f2f77
Harsh Depal <harshdepal@gmail.com>
2017-11-04 13:23:35 +0530

    initialized views for company

--- PlacementPortal/company/views.py
+++ PlacementPortal/company/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def login(request):
+	return HttpResponse("Login Page")
+
+def logout(request):
+	return HttpResponse("Logout Page")
+
+def register(request):
+	return HttpResponse("Registration Page")
