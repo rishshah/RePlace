2e175a4c14f76249509e70c2d801692015192da6
Harsh Depal <harshdepal@gmail.com>
2017-11-04 13:47:53 +0530

    created html files for company

--- PlacementPortal/company/views.py
+++ PlacementPortal/company/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def login(request):
-	return HttpResponse("Login Page")
+	return render(request, "company/login.html")
 
 def logout(request):
-	return HttpResponse("Logout Page")
+	return render(request, "company/login.html")
 
 def register(request):
-	return HttpResponse("Registration Page")
+	return render(request, "company/register.html")
