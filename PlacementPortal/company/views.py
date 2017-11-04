d2d9c369a20172add37f0876b574ea47074c2dff
Harsh Depal <harshdepal@gmail.com>
2017-11-04 15:04:47 +0530

    added login validation

--- PlacementPortal/company/views.py
+++ PlacementPortal/company/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def login(request):
-	return render(request, "company/login.html")
+	if (request.method == 'GET'):
+		return render(request, "company/login.html")
+	elif (request.method == 'POST'):
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username = username, password = password)
+		if (user is not None):
+			login(request, user)
+			return HttpResponse("Login Success")
+		else:
+			return HttpResponse("Invalid Credentials")
 
+@login_required(login_url = '/company/login')
 def logout(request):
-	return render(request, "company/login.html")
+	logout(request)
+	return HttpResponse("Logout Success")
+	#return render(request, "company/login.html")
+
 
 def register(request):
 	return render(request, "company/register.html")
