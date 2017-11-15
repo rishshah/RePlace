f7810e36f3bb95266eb0afcadb33bfd45566adee
rrriiissshhh <rishrocks17@gmail.com>
2017-11-16 03:22:47 +0530

    removed redundant static and template files

--- PlacementPortal/PlacementPortal/settings.py
+++ PlacementPortal/PlacementPortal/settings.py
@@ -15,6 +15,7 @@ import django
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__));
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'PlacementPortal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(PROJECT_ROOT, "__shared__/templates/"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, "__shared__/"),
+]
 
 STATIC_URL = '/static/'
+
 LOGIN_URL = '/replace'
 django.setup()
