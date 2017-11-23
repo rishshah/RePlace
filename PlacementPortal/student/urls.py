06f3fbada4ed8a89fa43387cbc940426de2787af
rrriiissshhh <rishrocks17@gmail.com>
2017-11-23 23:50:34 +0530

    final selections tab created

--- PlacementPortal/student/urls.py
+++ PlacementPortal/student/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'upload_resume/$', views.upload_resume, name='upload_resume'),
     url(r'related_jafs/(?P<jaf_id>[0-9]+)$', views.related_jaf_view, name='related_jaf_view'),
     url(r'unsign/$', views.unsign_jaf, name='unsign_jaf'),
+    url(r'selections/$', views.selections, name='selections'),
     url(r'sign/$', views.sign_jaf, name='sign_jaf'),
     url(r'logout/$', views.logout, name='logout'),
     url(r'jobs/$', views.my_jobs, name='my_jobs'),
